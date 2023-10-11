551e8d0b2c92f57b97f3a3dfcfeef12c15c37efe
PoliZyh <272798802@qq.com>
2023-10-11 18:49:49 +0800

    feat: 组件分类

--- src/views/Editor/components/ComponentList.vue
+++ src/views/Editor/components/ComponentList.vue
@@ -1,14 +1,27 @@
 <template>
     <div class="component-list-box" >
-        <div class="component-item"
-        v-for="(item, index) in componentList"
-        :key="item.componentName"
-        :data-index="index"
-        draggable="true"
-        @dragstart="handleDragStart"
-        >
-            {{ item.label }}
-        </div>
+
+        <el-collapse>
+            <el-collapse-item title="基础组件">
+                <div class="row">
+                    <div class="component-item"
+                    v-for="(item, index) in componentList"
+                    :key="item.componentName"
+                    :data-index="index"
+                    draggable="true"
+                    @dragstart="handleDragStart"
+                    >
+                        {{ item.label }}
+                    </div>
+                </div>
+            </el-collapse-item>
+
+            <el-collapse-item title="基础形状">
+
+            </el-collapse-item>
+
+        </el-collapse>
+
     </div>
 </template>
 
@@ -39,18 +52,22 @@ const handleDragStart = (e: DragEvent) => {
     height: 100%;
     background-color: white;
     border-right: 1px solid var(--border-c-deep);
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    .component-item {
-        height: fit-content;
-        padding: 10px 20px;
-        font-size: 0.8rem;
-        border: 1px solid rgb(213, 213, 213);
-        cursor: grab;
-        background-color: white;
-        position: relative;
+    padding: 10px 10px;
+    .row {
+        display: flex;
+        justify-content: space-around;
+        .component-item {
+            height: fit-content;
+            padding: 7px 15px;
+            font-size: 0.8rem;
+            border: 1px solid rgb(213, 213, 213);
+            cursor: grab;
+            background-color: white;
+            position: relative;
+            width: 40%;
+            text-align: center;
+
+        }
     }
 }
 </style>
